8f484d0ec68e8f7071a891810888be292fb41a4b
maria zakharova <maryzakharova0103@gmail.com>
2025-04-09 16:13:12 +0300

    add new directory src

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import _ from "lodash";
+
+const getGreating = () => {
+// console.log(_.last(["one", "two"]));
+// console.log("Hello, Hexlet!")
+const greating = 'Hello, Hexlet!' + '23';
+return greating;
+}
+export default getGreating;
