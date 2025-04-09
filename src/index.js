af99cb20b3e6ae91b3c7d1628da460128b5b058a
maria zakharova <maryzakharova0103@gmail.com>
2025-04-09 18:30:59 +0300

    installation eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 const getGreating = () => {
 // console.log(_.last(["one", "two"]));
 // console.log("Hello, Hexlet!")
-const greating = 'Hello, Hexlet!' + '23';
-return greating;
-}
+  const greating = 'Hello, Hexlet!' + '23';
+  return greating;
+};
 export default getGreating;
